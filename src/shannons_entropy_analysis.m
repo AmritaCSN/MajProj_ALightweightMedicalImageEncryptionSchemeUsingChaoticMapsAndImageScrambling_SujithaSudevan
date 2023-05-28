0cf543367aa973a6d0055a0f68c897891700d38b
Sujitha Sudevan <88279642+Sujitha6496@users.noreply.github.com>
2023-05-28 14:48:28 +0530

    Delete src directory

--- src/shannons_entropy_analysis.m
+++
@@ -1,8 +0,0 @@
-img = imread('encrypted_image_1.png');
-
-histogram = imhist(img);
-pdf = histogram / numel(img);
-
-H = -sum(pdf .* log2(pdf));
-
-disp(['Shannon''s entropy of encrypted image: ' num2str(H)]);
